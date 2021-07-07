2417fd4a2564a3f637b1d206d8cf86b0cedf54bf
SV-0 <sakshiverma8100@gmail.com>
2021-07-08 00:14:55 +0530

    changed models

--- server/models/PostModal.js
+++ server/models/PostModal.js
@@ -1,38 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const jobDescSchema = mongoose.Schema({
-    Info: String,
-    Eligibility: String,
-    ReqSkills: String,
-    Rewards: String
+  Info: String,
+  Eligibility: String,
+  ReqSkills: String,
+  Rewards: String,
 });
 
 const locationSchema = mongoose.Schema({
-    City: String,
-    State: String,
-    Country: String
-})
-
-const postSchema = mongoose.Schema({
-    jobTitle: String,
-    jobType: String,
-    postedBy: String,
-    duration: String,
-    salary: String,
-    company: String,
-    description: {
-        type: [jobDescSchema]
-    },
-    applyLink: String,
-    Location: {
-        type: [locationSchema]
-    },
-    createdAt: {
-        type: Date,
-        default: new Date()
-    }
+  City: String,
+  State: String,
+  Country: String,
 });
 
-const PostMessage = mongoose.model('PostMessage', postSchema);
+const postSchema = mongoose.Schema({
+  jobTitle: String,
+  jobType: String,
+  postedBy: String,
+  duration: String,
+  salary: String,
+  company: String,
+  description: {
+    type: { jobDescSchema },
+    default: {},
+  },
+  applyLink: String,
+  Location: {
+    type: { locationSchema },
+    default: {},
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
