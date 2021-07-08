5a3e37cc4ac9f35543750ee0433268417fc6d950
SV-0 <sakshiverma8100@gmail.com>
2021-07-08 18:30:13 +0530

    add actions for job posts

---
+++ client/src/actions/jobs.js
@@ -0,0 +1,65 @@
+import * as api from "../api/index";
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from "../constants/actionTypes";
+
+export const getJobs = () => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchJobs(); //response=data object
+    dispatch({ type: FETCH_ALL, payload: data });
+    // const action = { type: 'FETCH_ALL', payload: [] };
+    // dispatch (action);
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getJobsBySearch = (searchQuery) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const {
+      data: { data },
+    } = await api.fetchJobsBySearch(searchQuery);
+    dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createJob = (job, history) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.createJob(job);
+    history.push("./");
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateJob = (id, job, history) => async (dispatch) => {
+  try {
+    const { data } = await api.updateJob(id, job);
+    dispatch({ type: UPDATE, payload: data });
+    history.push("./");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deleteJob = (id) => async (dispatch) => {
+  try {
+    await api.deleteJob(id);
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const likeJob = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likeJob(id);
+
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
