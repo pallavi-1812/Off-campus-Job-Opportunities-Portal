ccad0193fdc38bb71a4c80cef4aedd2cc810ce47
SV-0 <sakshiverma8100@gmail.com>
2021-07-08 18:39:29 +0530

    add form controls

---
+++ client/src/components/Controls/Controls.js
@@ -0,0 +1,11 @@
+import RadioGroup from "./RadioGroup";
+import Select from "./Select";
+import Checkbox from "./Checkbox";
+
+const Controls = {
+  RadioGroup,
+  Select,
+  Checkbox,
+};
+
+export default Controls;
