2d13c64ac9454cb51292d849ee17e86111c12f46
SV-0 <sakshiverma8100@gmail.com>
2021-07-08 18:27:11 +0530

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-    return (
-        <div>
-            Hello
-        </div>
-    )
-}
-
+  const user = JSON.parse(localStorage.getItem("profile"));
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={() => <Redirect to="/jobs" />} />
+        <Route path="/jobs" exact component={Home} />
+        <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/jobs" />)} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
