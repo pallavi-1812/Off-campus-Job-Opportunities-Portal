ae7f49fb6b5fd7555770c1b77e1c4bdfa9dadb8f
SV-0 <sakshiverma8100@gmail.com>
2021-07-24 19:33:42 +0530

    added redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 import App from "./App";
 import "./index.css";
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const middleware = [thunk];
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
+// const store = createStore(reducers, compose(applyMiddleware(thunk)), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
