f73b3893099778abe5e7556850747d65ebdfac02
pallavi1812 <upallavi1812@gmail.com>
2021-07-24 15:39:32 +0530

    added proper padding in posts' container

--- client/src/components/Jobs/styles.js
+++ client/src/components/Jobs/styles.js
@@ -3,66 +3,12 @@ import { deepPurple } from "@material-ui/core/colors";
 
 export default makeStyles((theme) => ({
   container: {
-    paddingTop: "20px",
-    paddingLeft: "80px",
-    paddingRight: "160px",
+    padding: "1.5rem 8rem 0",
     [theme.breakpoints.down("md")]: {
-      padding: "10px 10px",
+      padding: "1.5rem 4rem 0",
     },
-  },
-  heading: {
-    color: "rgba(0,183,255, 1)",
-    textDecoration: "none",
-  },
-  image: {
-    marginLeft: "15px",
-  },
-  toolbar: {
-    display: "flex",
-    justifyContent: "flex-end",
-    width: "400px",
-  },
-  profile: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "400px",
-  },
-  userName: {
-    display: "flex",
-    alignItems: "center",
-  },
-  brandContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
-  smMargin: {
-    margin: theme.spacing(1),
-  },
-  purple: {
-    color: theme.palette.getContrastText(deepPurple[500]),
-    backgroundColor: deepPurple[500],
-  },
-  [theme.breakpoints.down("sm")]: {
-    appBar: {
-      padding: "10px 20px",
+    [theme.breakpoints.down("sm")]: {
+      padding: "0.5rem 1rem 0",
     },
-    heading: {
-      display: "none",
-    },
-    userName: {
-      display: "none",
-    },
-    image: {
-      marginLeft: "5px",
-    },
-    toolbar: {
-      display: "flex",
-      justifyContent: "flex-end",
-      width: "160px",
-    },
-  },
-
-  actionDiv: {
-    textAlign: "center",
   },
 }));
