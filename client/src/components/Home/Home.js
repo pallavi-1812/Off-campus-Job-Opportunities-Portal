8d8bf1c43873b01c6837a8d9e88e7bd52bd66f3c
pallavi1812 <upallavi1812@gmail.com>
2021-07-25 23:14:30 +0530

    removed tabs

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,77 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Fab } from "@material-ui/core";
+import { Grid, Fab, Container } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import AddIcon from "@material-ui/icons/Add";
 
 import { getJobs } from "../../actions/jobs";
 import useStyles from "./styles";
 import Jobs from "../Jobs/jobs";
-import Other from "../Jobs/Job/other";
 import Form from "../Form/form";
 import Filter from "../Filter/Filter";
-import { Tabs, Tab } from "@material-ui/core";
 import SearchBar from "../SearchBar/SearchBar";
 
-const Home = (props) => {
+const Home = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const [currentId, setCurrentId] = useState(null);
   const [openPopup, setOpenPopup] = useState(false);
 
-  const { match, history } = props;
-  const { params } = match;
-  const { page } = params;
-
-  const tabNameToIndex = {
-    0: "tpc",
-    1: "others",
-  };
-
-  const indexToTabName = {
-    tpc: 0,
-    others: 1,
-  };
-  console.log(props);
-  console.log(params);
-  console.log(page);
-  const [selectedTab, setSelectedTab] = useState(indexToTabName[page]);
-
-  const handleChange = (event, newValue) => {
-    history.push(`/jobs/${tabNameToIndex[newValue]}`);
-    setSelectedTab(newValue);
-  };
-
   const user = JSON.parse(localStorage.getItem("profile"));
   useEffect(() => {
     dispatch(getJobs());
   }, [currentId]);
+
   return (
     <>
+      {user?.result?.role === "1" && (
+        <Fab size="medium" color="primary" style={{ position: "absolute", right: "10px", zIndex: "100" }} onClick={() => setOpenPopup(true)}>
+          <AddIcon />
+        </Fab>
+      )}
+      <Form openPopup={openPopup} currentId={currentId} setOpenPopup={setOpenPopup} setCurrentId={setCurrentId} />
       <Grid className={classes.gridContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-        <Grid item xs={12} sm={12} md={3} lg={3}>
+        <Grid item xs={12} sm={12} md={12} lg={3}>
           <Filter />
         </Grid>
-        <Grid item xs={12} sm={12} md={9} lg={9}>
-          <Grid className={classes.searchBar}><SearchBar /></Grid>
-          <Grid>
-            <Tabs value={selectedTab} onChange={handleChange}>
-              <Tab label="tpc" />
-              <Tab label="others" />
-            </Tabs>
-            {selectedTab === 0 && (
-              <>
-                {user?.result?.role === "1" && (
-                  <Fab size="medium" color="primary" style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: "100" }} onClick={() => setOpenPopup(true)}>
-                    <AddIcon />
-                  </Fab>
-                )}
-                <Form openPopup={openPopup} currentId={currentId} setOpenPopup={setOpenPopup} setCurrentId={setCurrentId} />
-                <Jobs setCurrentId={setCurrentId} openPopup={openPopup} setOpenPopup={setOpenPopup} />
-              </>
-            )}
-            {selectedTab === 1 && <Other />}
+        <Container maxWidth="md">
+          <Grid item>
+            <Grid className={classes.searchBar}><SearchBar /></Grid>
           </Grid>
-        </Grid>
+          <Grid item>
+            <Jobs setCurrentId={setCurrentId} openPopup={openPopup} setOpenPopup={setOpenPopup} />
+          </Grid>
+        </Container>
       </Grid>
     </>
   );
